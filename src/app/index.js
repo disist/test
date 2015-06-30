d32262cb67d4000ac4b17727cb7922822e63b4ca
Dima <disist357@gmail.com>
2015-07-01 01:02:52 +0300

    Remove bootstrap. Add angular-material

--- src/app/index.js
+++ src/app/index.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module('chat', ['ui.router'])
+  angular.module('chat', ['ui.router', 'ngMaterial'])
     .config(function ($stateProvider, $urlRouterProvider) {
 
       $urlRouterProvider.rule(function($injector, $location) {
